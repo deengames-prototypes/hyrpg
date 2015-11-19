aceb43076983e85379eb1fb51b1381e003213cab
ashes999 <ashes999@yahoo.com>
2015-11-19 16:36:07 -0500

    Click to attack; random damage; enemies can die.

--- prototype/main.js
+++ prototype/main.js
@@ -3,21 +3,41 @@ Crafty.c('Player', {
     this.requires('Actor, Text2')
       .text('HP: 50')
       .size(64, 64).move(64, 64).color('blue');
+  },
+
+  getDamage: function() {
+    // 8-12
+    return Math.round(8 + Math.random(4));
   }
 })
 
 Crafty.c('Enemy', {
   init: function() {
-    this.requires('Actor, Text2')
-      .text('30')
-      .size(40, 40).color('red');
+    var self = this;
+    this.requires('Actor, Text2').color('red');
+      self.hp = 30;
+      this.click(function() {
+        damage = Crafty('Player').getDamage();
+        self.hp -= damage;
+        self.refresh();
+      });
+
+      self.refresh();
+  },
+
+  refresh: function() {
+    this.text(this.hp);
+    this.size(40, 40);
+    if (this.hp <= 0) {
+      this.destroy();
+    }
   }
 })
 
 Game = {
   start: function() {
     Crafty.init(720, 405);
-    Crafty.background('black');
+    Crafty.background('#4A4');
     Crafty.e('Player');
     // Fake enemy
     Crafty.e('Enemy').move(640, 64);
