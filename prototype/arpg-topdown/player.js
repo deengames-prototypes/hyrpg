ff46906bc13449eee43486f16e017b1a60668dab
ashes999 <ashes999@yahoo.com>
2015-12-04 17:53:15 -0500

    Attacking in four directions! for #4

--- prototype/arpg-topdown/player.js
+++ prototype/arpg-topdown/player.js
@@ -5,21 +5,36 @@ Crafty.c('Sword', {
     var self = this;
     var player = Crafty('Player');
     var myX;
+    var myY;
     var endRotation;
 
-    if (player.direction == 'right') {
-      this.origin('middle left');
-      myX = player.attr('x') + player.attr('w');
-      this.rotation = -45;
-      endRotation = 45;
-    } else if (player.direction == 'left') {
-      this.origin('middle right');
-      myX = player.attr('x') - this.attr('w');
-      this.rotation = 45;
-      endRotation = -45;
+    if (player.direction == 'left' || player.direction == 'right') {
+      if (player.direction == 'right') {
+        this.origin('middle left');
+        myX = player.attr('x') + player.attr('w');
+        this.rotation = -45;
+        endRotation = 45;
+      } else if (player.direction == 'left') {
+        this.origin('middle right');
+        myX = player.attr('x') - this.attr('w');
+        this.rotation = -45;
+        endRotation = 45;
+      }
+      this.move(myX, player.attr('y') + (player.attr('h') / 2));
+    } else {
+      if (player.direction == 'down') {
+        this.origin(0, this.attr('h') / 2);
+        myY = player.attr('y') + player.attr('h');
+        this.rotation = 45;
+        endRotation = 135;
+      } else if (player.direction == 'up') {
+        this.origin(0, this.attr('h') / 2);
+        myY = player.attr('y');
+        this.rotation = -135;
+        endRotation = -45;
+      }
+      this.move(player.attr('x') + (player.attr('w') / 2), myY);
     }
-
-    this.move(myX, player.attr('y') + (player.attr('h') / 2));
 
     this.tween({ rotation: endRotation }, 0.25);
     this.one('TweenEnd', function() {
@@ -43,11 +58,9 @@ Crafty.c('Player', {
 
     // fourway seems to take Z and ignore the numeric keypad. Use X, C, and V
     this.keyPress('X', function() {
-      if (self.direction == 'left' || self.direction == 'right') {
-        // Assume right for testing
-        self.disableControl();
-        Crafty.e('Sword');
-      }
+      // Assume right for testing
+      self.disableControl();
+      Crafty.e('Sword');
     });
 
     // Keep track of direction
